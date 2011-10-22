f09768021036bfc5d53de47d690568638505ad33
Charles Desouza <charles.desouza3000@gmail.com>
2011-10-22 03:03:20 -0400

    Created my first server and routing functionality

---
+++ server.js
@@ -0,0 +1,19 @@
+var http = require( "http" );
+var url = require( "url" );
+
+
+function start(route) {
+	http.createServer( function( request, response ) {
+		var pathname = url.parse( request.url ).pathname;
+		console.log( "Request for " + pathname + "received" );
+		
+		route( pathname );
+
+		response.writeHead( 200, { "Content-Type": "text/plain" } );
+		response.write( "Hello World" );
+		response.end();
+	}).listen( 8888 );
+
+	console.log( "Server has started." );
+}
+exports.start = start;
