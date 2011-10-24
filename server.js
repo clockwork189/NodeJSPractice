774381d4a91c0dad4b5fe3e6f872d4bc317ce80f
Charles Desouza <charles.desouza3000@gmail.com>
2011-10-24 01:35:57 -0400

    couple mods

--- server.js
+++ server.js
@@ -5,11 +5,10 @@ var url = require( "url" );
 function start( route, handle ) {
 	http.createServer( function( request, response ) {
 		var pathname = url.parse( request.url ).pathname;
-		console.log( "Request for " + pathname + "received" );
-		
-		route( handle, pathname );
+		console.log( "Request for " + pathname + " received" );
 
 		response.writeHead( 200, { "Content-Type": "text/plain" } );
+		var content = route( handle, pathname );
 		response.write( "Hello World" );
 		response.end();
 
